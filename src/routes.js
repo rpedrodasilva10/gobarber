834fd32a3354aef52b0d69a20ef226530e14a2b8
Renan Pedro da Silva <rpedrodasilva10@gmail.com>
2020-02-05 23:43:56 -0300

    feat: UserController store method

--- src/routes.js
+++ src/routes.js
@@ -1,23 +1,8 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get(
-  '/',
-  async (req, res) => {
-    console.log('--------------------------------- T E S T E  ');
-    /*name: Sequelize.STRING,
-        email: Sequelize.STRING,
-        password_hash: Sequelize.STRING, */
-    const user = await User.create({
-      name: 'Renan',
-      email: 'rpedrodasilva10@gmail.com',
-      password_hash: '12313131',
-    });
-    return res.json(user);
-  },
-  err => console.error(err)
-);
+routes.post('/users', UserController.store);
 
 export default routes;
