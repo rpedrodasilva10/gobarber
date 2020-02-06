f53114b5279e8c7a65230a4ca3a6cb31ea21ed28
Renan Pedro da Silva <rpedrodasilva10@gmail.com>
2020-02-05 23:16:19 -0300

    feat: create user model with sequelize ORM

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,12 @@
 module.exports = {
-  dialect: 'postgres',
   host: 'localhost',
   username: 'postgres',
   password: 'docker',
   database: 'gobarber',
+  dialect: 'postgres',
   define: {
     timestamps: true,
     underscored: true,
-    undescoredAll: true,
+    underscoredAll: true,
   },
 };
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,23 @@
-import Router from 'express';
+import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ hello: 'world' });
-});
+routes.get(
+  '/',
+  async (req, res) => {
+    console.log('--------------------------------- T E S T E  ');
+    /*name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password_hash: Sequelize.STRING, */
+    const user = await User.create({
+      name: 'Renan',
+      email: 'rpedrodasilva10@gmail.com',
+      password_hash: '12313131',
+    });
+    return res.json(user);
+  },
+  err => console.error(err)
+);
 
 export default routes;
