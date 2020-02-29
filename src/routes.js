4abf1eb45b9d27e942329ac55962feff966f0a17
Renan Pedro da Silva <rpedrodasilva10@gmail.com>
2020-02-29 13:50:20 -0300

    fix: avatar file upload

--- src/routes.js
+++ src/routes.js
@@ -4,18 +4,30 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
+import FileController from './app/controllers/FileController';
 
+import multer from 'multer';
+import multerConfig from './config/multer';
 import authMiddleware from './app/middlewares/auth';
 
+const upload = multer(multerConfig);
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
+
 routes.put('/users', UserController.update);
+
 routes.get('/providers', ProviderController.index);
+
+routes.post('/files', upload.single('file'), FileController.store);
+
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedules', ScheduleController.index);
 
 export default routes;
