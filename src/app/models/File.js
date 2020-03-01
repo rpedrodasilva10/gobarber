4fcac7397fd65541ad7ec516a3bfec3bb4fd3960
Renan Pedro da Silva <rpedrodasilva10@gmail.com>
2020-02-29 21:32:25 -0300

    fix: bugfix on file upload

--- src/app/models/File.js
+++ src/app/models/File.js
@@ -9,15 +9,7 @@ class File extends Model {
         url: {
           type: Sequelize.VIRTUAL,
           get() {
-<<<<<<< HEAD
-<<<<<<< HEAD
             return `http://localhost:3000/files/${this.path}`;
-=======
-            return `http://localhost:3333/files/${this.path}`;
->>>>>>> 97459f4... Add index providers with avatar file url
-=======
-            return `http://localhost:3000/files/${this.path}`;
->>>>>>> 4d31c56... feat: create notifications and mongodb connection
           },
         },
       },
